94838ef112e9eb3135ec7d792ea04ba085779b46
Sanjana Jayasooriya <135824601+SanjanaJaya@users.noreply.github.com>
2025-03-22 09:38:15 +0530

    Reapply "new Garbage & Health Pages Added"
    This reverts commit 813c3176ff2d29ca44f99fcf3d5d1212ee46d09a.

---
+++ lib/garbage.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+
+class GarbagePage extends StatelessWidget {
+  const GarbagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[100],
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Garbage',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(text: 'Collection schedule', onPressed: () {}),
+            const SizedBox(height: 30),
+            CustomButton(text: 'Request pickup', onPressed: () {}),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(LucideIcons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(LucideIcons.camera), label: ''),
+          BottomNavigationBarItem(icon: Icon(LucideIcons.bell), label: ''),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300, // Increased width
+      height: 80, // Increased height
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue[800],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
