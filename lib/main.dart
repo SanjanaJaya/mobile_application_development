7c08889b15d2025f46b7bcfd3d18ec6ddec2fcf4
Sanjana Jayasooriya <135824601+SanjanaJaya@users.noreply.github.com>
2025-03-23 13:29:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,9 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
@@ -42,7 +45,7 @@ class DashboardScreen extends StatelessWidget {
         elevation: 0,
         leading: Builder(
           builder: (context) => IconButton(
-            icon: Icon(Icons.menu, color: Colors.black),
+            icon: Icon(Icons.menu, color: Colors.transparent),
             onPressed: () {
               Scaffold.of(context).openDrawer();
             },
@@ -53,7 +56,6 @@ class DashboardScreen extends StatelessWidget {
             padding: EdgeInsets.only(right: 16.0),
             child: GestureDetector(
               onTap: () {
-                // Navigate to ProfileScreen when the profile picture is clicked
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -67,68 +69,6 @@ class DashboardScreen extends StatelessWidget {
             ),
           ),
         ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-              ),
-              accountName: Text(
-                userData['Name'],
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              accountEmail: Text(
-                userData['Email'],
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(userData['image']),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.location_on, color: Colors.black),
-              title: Text('Location'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.contact_mail, color: Colors.black),
-              title: Text('Contact us'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ContactUsScreen(), // Navigate to ContactPage
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings, color: Colors.black),
-              title: Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.help_outline, color: Colors.black),
-              title: Text('Help and FAQs'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 16.0),
@@ -149,47 +89,33 @@ class DashboardScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              child: Row(
-                children: [
-                  Icon(Icons.search, color: Colors.grey[600]),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Search here',
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
             ),
-            SizedBox(height: 50),
+            SizedBox(height: screenHeight * 0.05), // Use percentage of screen height
             Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'Hello,', // First line
+                  'Hello,',
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: screenWidth * 0.08, // Responsive font size
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 Text(
-                  userData['Name'], // Second line
+                  userData['Name'],
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: screenWidth * 0.08, // Responsive font size
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 80),
+            SizedBox(height: screenHeight * 0.08), // Use percentage of screen height
             GridView.count(
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
               crossAxisCount: 3,
-              childAspectRatio: 0.9, // Adjust this value to give more height to the children
+              childAspectRatio: 0.9,
               children: [
                 ServiceIcon('img/book.png', 'Library', onTap: () {
                   Navigator.push(
@@ -203,13 +129,13 @@ class DashboardScreen extends StatelessWidget {
                     MaterialPageRoute(builder: (context) => GarbagePage()),
                   );
                 }),
-                ServiceIcon('img/assessment.png', 'Assessment', onTap: () {
+                ServiceIcon('img/assessment.png', 'Assessments', onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => AssessmentPage()),
                   );
                 }),
-                ServiceIcon('img/street.png', 'Street Maintenance', onTap: () {
+                ServiceIcon('img/street.png', 'Maintenances', onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => StreetPage()),
@@ -224,12 +150,12 @@ class DashboardScreen extends StatelessWidget {
                 ServiceIcon('img/contact.png', 'Contact Us', onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => ContactUsScreen()), // Navigate to ContactPage
+                    MaterialPageRoute(builder: (context) => ContactUsScreen()),
                   );
                 }),
               ],
             ),
-            SizedBox(height: 30),
+            SizedBox(height: screenHeight * 0.04), // Use percentage of screen height
           ],
         ),
       ),
@@ -246,15 +172,17 @@ class ServiceIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        height: 120, // Fixed height to ensure enough space
+        height: screenWidth * 0.3, // Responsive height
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              padding: EdgeInsets.all(20),
+              padding: EdgeInsets.all(screenWidth * 0.05), // Responsive padding
               decoration: BoxDecoration(
                 color: Colors.white,
                 shape: BoxShape.circle,
@@ -267,13 +195,13 @@ class ServiceIcon extends StatelessWidget {
                   ),
                 ],
               ),
-              child: Image.asset(imagePath, width: 60, height: 60),
+              child: Image.asset(imagePath, width: screenWidth * 0.15, height: screenWidth * 0.15), // Responsive image size
             ),
-            SizedBox(height: 10),
-            Flexible( // Ensure text doesn't overflow
+            SizedBox(height: screenWidth * 0.02), // Responsive spacing
+            Flexible(
               child: Text(
                 label,
-                style: TextStyle(fontSize: 14),
+                style: TextStyle(fontSize: screenWidth * 0.035), // Responsive font size
                 textAlign: TextAlign.center,
               ),
             ),
